d33784140ed5a2954c0c60a6696f795aa7c83523
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2019-12-02 06:21:56 -0800

    Update my-grep.c

--- my-grep.c
+++ my-grep.c
@@ -2,7 +2,8 @@
  * Käyttöjärjestelmät ja systeemiohjelmointi
  * Harjoitustyö osa 1/?
  * Syksy 2019
- * Rami Saarivuori
+ * Rami Saarivuori & Aarne Savolainen
+ * my-grep, hakee tiedostoista hakusanaa
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,37 +11,38 @@
 
 
 int main(int argc, char *argv[]) {
-	if (argc == 1) {
+	if (argc == 1) { //jos argumentteja ei ole annettu, tulostetaan käyttöohje ja lopetetaan
 		printf("my-grep: searchterm [file ...]\n");
 		exit(0);
 	};
-	char buffer[150], *cp;
-	if (argc == 2) {
-		fgets(buffer, 150, stdin);
-		cp = strstr(buffer, argv[1]);
-		if (cp == NULL) {
+	char buffer[250], *cp; //lukubufferi ja hakumuuttuja
+	if (argc == 2) { //vain hakusana
+		fgets(buffer, 250, stdin); //koska vain hakusana, luetaan stdin streamistä
+		cp = strstr(buffer, argv[1]); //haetaan annettua hakusanaa ja tulos tallennetaan muuttujaan
+		if (cp == NULL) { //jos hakusanaa ei löydä annetusta merkkijonosta, lopetetaan
 			exit(0);
-		} else {
+		} else { //hakusana löytyi, tulostetaan koko bufferi
 			printf("%s", buffer);
 			exit(0);
 	};};
 
 	FILE *fp;
-	char* buff = NULL;
-	size_t n;
-	ssize_t a;
-	for (int i=2;i<argc;i++) {
-		fp = fopen(argv[i], "r");
-		if (fp == NULL) {
+	char* buff = NULL; //Lukubufferi
+	size_t n; 
+	ssize_t a; //tähän tallennetaan merkkien määrä jonka getline() antaa
+	for (int i=2;i<argc;i++) { //useampi tiedosto, joten silmukka
+		fp = fopen(argv[i], "r"); //avataan tiedosto
+		if (fp == NULL) { //avaus epäonnistui, tulostetaan virheilmoitus ja lopetetaan
 			printf("my-grep: cannot find file %s", argv[i]);
+			exit(1);
 		};
-		a = getline(&buff, &n, fp);
-		while (a >= 0) {
-			cp = strstr(buff, argv[1]);
+		a = getline(&buff, &n, fp); //haetaan tiedostosta ensimmäinen rivi
+		while (a >= 0) { //silmukka kunnes rivilllä ei ole enää merkkejä (tiedoston loppu)
+			cp = strstr(buff, argv[1]); //haetaan vastaavuutta hakusanalle
 			if (cp != NULL) {
 				printf("%s", buff);
 			};
-			a = getline(&buff, &n, fp);
+			a = getline(&buff, &n, fp); //haetaan seuraava rivi
 		};};
 		fclose(fp);
 		return 0;
