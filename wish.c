3a66341edaee11ee4e192ba584dc2d21fa2a16bb
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2019-12-02 07:51:53 -0800

    Update wish.c

--- wish.c
+++ wish.c
@@ -13,10 +13,39 @@
 void parser (char *);
 char* concat(const char *, const char *);
 
-int main(int argc, char * argv[]) {
-	
-	if (argc == 2) { //batch mode
-		printf("Tiedosto");
+int main(int argc, char *argv[]) {
+	if ( argc > 2 ) { /* Liikaa argumentteja */
+		printf("Too many arguments. There should be at most one argument.\n");
+		exit(1);
+	} else if ( argc == 2 ) { /*Bash mode, luetaan tekstitiedostosta käskyt */
+		char *line_buf = NULL;
+		size_t line_buf_size = 0;
+		int line_count = 0;
+		ssize_t line_size;
+
+
+
+		FILE *fp = fopen(argv[1], "r");
+
+			if (fp == NULL) {
+				printf("Error: cannot open file %s\n", argv[1]);
+				exit(1);
+			}
+
+			printf("Reading file %s\n", argv[1]);
+
+			line_size = getline(&line_buf, &line_buf_size, fp);
+			while (line_size >= 0) {
+				line_count++;
+
+				line_size = getline(&line_buf, &line_buf_size, fp);
+			}
+
+			free(line_buf);
+			line_buf = NULL;
+
+			fclose(fp);
+			exit(0);
 	} else {
 		char* buff = NULL; //lukupuskuri
 		size_t n;
@@ -51,8 +80,9 @@ void parser(char strings[]) {
 
 char* concat(const char *s1, const char *s2) //merkkijonojen yhdistämiseen tarkoitettu funktio
 { //https://stackoverflow.com/questions/8465006/how-do-i-concatenate-two-strings-in-c
-    if (char *result = malloc(strlen(s1) + strlen(s2) + 1)==NULL) { // +1 for the null-terminator
-		printf("Memory allocation failed\n");
+	char *result;
+    if ((result = malloc(strlen(s1) + strlen(s2) + 1))==NULL) { // +1 for the null-terminator
+		printf("Memory allocation failed");
 		exit(1);
 	};
     strcpy(result, s1);
