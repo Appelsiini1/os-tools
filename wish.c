f82de28d63fb47ecf2c966ad234c97ca8330be85
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2019-12-02 16:37:28 +0200

    Add files via upload

---
+++ wish.c
@@ -0,0 +1,10 @@
+/*
+ * Käyttöjärjestelmät ja systeemiohjelmointi
+ * Harjoitustyö osa 5/6
+ * Syksy 2019
+ * Rami Saarivuori & Aarne Savolainen
+ * Basic shell: wish
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
