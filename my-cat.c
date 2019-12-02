a3b783a6f7a63205093e36dfd86251f1ca5e6b31
Appelsiini1 <37345715+Appelsiini1@users.noreply.github.com>
2019-12-02 06:20:47 -0800

    Update my-cat.c

--- my-cat.c
+++ my-cat.c
@@ -3,33 +3,34 @@
  * Harjoitustyö osa 1/6
  * Syksy 2019
  * Rami Saarivuori & Aarne Savolainen
+ * my-cat, tulostaa tiedoston sisällön näytölle
 */
 #include <stdio.h>
 #include <stdlib.h>
 
 
-int main(int argc, char *argv[]) { 
-	if (argc == 1) {
+int main(int argc, char *argv[]) {
+	if (argc == 1) { //Jos argumentteja ei ole annettu, lopetetaan
 		exit(0);
 	};
 	FILE *fp;
-	char s[50];
-	for (int i=0;i<(argc-1);i++) {
+	char s[50]; //lukubufferi
+	for (int i=0;i<(argc-1);i++) { //silmukka jotta voidaan tulostaa useampi tiedosto, jos annettu. argc-1, koska ensimmäinen on ohjelma oma nimi
 		fp = fopen(argv[i+1], "r");
 
 		if (fp == NULL) {
-			printf("Cannot open file %s\n", argv[i+1]);
+			printf("Cannot open file %s\n", argv[i+1]); //Jos ei voida avata tiedostoa syystä tai toisesta, tulostetaan virheilmoitus ja lopetetaan. 
 			exit(1);
 		};
-		while (s != NULL) {
-		if (fgets(s, 50, fp) != NULL ) {
+		while (s != NULL) { //tulostus
+		if (fgets(s, 50, fp) != NULL ) { //luetaan 50 merkkiä kerrallaan tiedostosta
 			printf("%s", s);
-		} else {
+		} else { //jos päästään tiedoston loppuun (fgets = NULL) lopetetaan silmukka
 			break;
 		};
 	};
 };
-	fclose(fp);
+	fclose(fp); //suljetaan tiedosto
 	
 	return 0;
 }
