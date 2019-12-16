8bf64b9e9535dce440303963aa85e5cc4dd9019c
Apezkin <51073470+Apezkin@users.noreply.github.com>
2019-12-16 15:40:51 +0200

    Update testi.c

--- xv6-sys/testi.c
+++ xv6-sys/testi.c
@@ -11,11 +11,11 @@
 #include "user.h"
 
 int main(void) {
-	printf(0, "Read: %d\n", getcount(0,0));
+	printf(0, "Read: %d\n", getcount(0,0)); /* Kutsuu read systeemikutsua () */
 	
-	printf(0, "Change to exit: %d\n", getcount(0, 2));
+	printf(0, "Change to exit: %d\n", getcount(0, 2)); /* Kutsuu exit systeemikutsua */
 	
-	printf(0, "Reset counter: %d\n", getcount(1, 0));
+	printf(0, "Reset counter: %d\n", getcount(1, 0)); /* Resettaa laskurin */
 	
 	exit();
 }
