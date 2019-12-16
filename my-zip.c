eecacb4c214286b1887df5dcb1c4d7d6c9aafbb0
Apezkin <51073470+Apezkin@users.noreply.github.com>
2019-12-16 15:21:58 +0200

    Update my-zip.c

--- my-zip.c
+++ my-zip.c
@@ -9,14 +9,14 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	if (argc == 1) {
+	if (argc == 1) { /* Jos argumentteja ei anneta, ohjelma tulostaa vain käytön */
 		printf("my-zip: file1 [file2] ... \n");
 		exit(0);
 	};
 	
 	int n = 0;
 	char buff1[2], buff2[2];
-	for (int i=1;i<(argc);i++){
+	for (int i=1;i<(argc);i++){ /* Käy annetut tiedostot läpi */
 		FILE *fp = fopen(argv[i], "r");
 		if (fp == NULL) {
 			printf("Cannot open file %s\n", argv[i]);
@@ -24,12 +24,12 @@ int main(int argc, char * argv[]) {
 		};
 		
 		buff1[0] = getc(fp);
-		buff2[0] = buff1[0];
+		buff2[0] = buff1[0]; /* Asetetaan samoiksi tarkistuksen takia */
 		while (buff1[0] != -1) {
-			if (buff2[0] == buff1[0]) {
+			if (buff2[0] == buff1[0]) { /* Jos samat, nostetaan laskuria yhdellä */
 				n++;
 				buff1[0] = getc(fp);
-			} else {
+			} else { /* Jos eri, kirjoitetaan tiedostoon zipattu muoto, eli aaaa -> 4a */
 				fwrite(&n, sizeof(n), 1, stdout);
 				fwrite(&buff2, 1, 1, stdout);
 				n = 0;
