8b319def610dce4d7c62fa0d60d79ff9a6676921
Apezkin <51073470+Apezkin@users.noreply.github.com>
2019-12-16 15:36:39 +0200

    Update my-unzip.c

--- my-unzip.c
+++ my-unzip.c
@@ -9,14 +9,14 @@
 #include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	if (argc == 1) {
+	if (argc == 1) { /* Jos ei argumentteja, tulostaa ohjelman käytön */
 		printf("my-unzip: file1 [file2] ... \n");
 		exit(0);
 	};
 	
 	char buff2[1];
 	int buff1;
-	for (int i=1;i<argc;i++) {
+	for (int i=1;i<argc;i++) { /* Käy tiedostot läpi */
 		FILE *fp = fopen(argv[i], "r");
 		
 		if (fp == NULL) {
@@ -25,11 +25,11 @@ int main(int argc, char * argv[]) {
 		};
 		int b;
 		
-		fread(&buff1, sizeof(int), 1, fp);
-		b = fread(buff2, 1, 1, fp);
+		fread(&buff1, sizeof(int), 1, fp); /* Lukee numeron tiedostosta */
+		b = fread(buff2, 1, 1, fp); /* Lukee kirjaimen tiedostosta */
 		while (b != 0) {
 			
-			for (int a=0;a<=buff1;a++) {
+			for (int a=0;a<=buff1;a++) { /* Kirjoittaa kirjaimen luvun osoittaman määrän verran */
 				printf("%c", buff2[0]);
 			}
 			fread(&buff1, sizeof(int), 1, fp);
